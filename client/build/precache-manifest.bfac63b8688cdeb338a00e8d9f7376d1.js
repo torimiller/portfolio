9b7939dd924e37060837277286e4fefc30266986
Tori Miller <millertori04@gmail.com>
2020-06-22 20:30:24 -0700

    created production build

---
+++ client/build/precache-manifest.bfac63b8688cdeb338a00e8d9f7376d1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87dc4dfcd06b055bb8c28dcbff1691e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e2027318188cf5c057e",
+    "url": "/static/css/main.d55ccf51.chunk.css"
+  },
+  {
+    "revision": "28ccb6627e169c191b4a",
+    "url": "/static/js/2.56856462.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.56856462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e2027318188cf5c057e",
+    "url": "/static/js/main.4d5acd95.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "e9960e211e346fe80b240c9d3c0cc763",
+    "url": "/static/media/about.e9960e21.svg"
+  },
+  {
+    "revision": "847be94530703b50af2f6b268deccb3f",
+    "url": "/static/media/autolog.847be945.png"
+  },
+  {
+    "revision": "34836919f9a182520bc26198decc7064",
+    "url": "/static/media/contact.34836919.svg"
+  },
+  {
+    "revision": "b3f9f771d2ba7b14778fdac4ebec0131",
+    "url": "/static/media/portfolio.b3f9f771.svg"
+  },
+  {
+    "revision": "3e0791163da9554d5a7ced24edef1a8c",
+    "url": "/static/media/puppypal.3e079116.png"
+  },
+  {
+    "revision": "66c012bb707d5e4aa4c5205d915642c9",
+    "url": "/static/media/twitter.66c012bb.png"
+  }
+]);
