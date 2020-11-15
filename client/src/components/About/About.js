d2bdaf3fda1c21c94de38a354953c892e15bc790
Tori Miller <millertori04@gmail.com>
2020-11-15 13:44:39 -0800

    updated resume

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -6,10 +6,10 @@ const About = () => {
     return (
         <Fragment>
             <div className="about-wrapper">
-                <h1 className="about-h1">Full Stack Web Developer</h1>
+                {/* <h1 className="about-h1">Full Stack Web Developer</h1> */}
                 <div className="about-col-left">
-                    <h2 className="about-h2">I'm a full stack web developer with over a year of experience 
-                    building web applications from scratch, developing and integrating APIs, and working with databases.</h2>
+                    <h1 className="about-h1">Full stack web developer with experience 
+                    building web applications from scratch, developing and integrating APIs, and working with databases.</h1>
                     <p className="about-body-copy">I’m a well-organized, dedicated, and versatile developer with a strong passion for technology and design. I’ve worn many hats in my career between design, marketing, project management and sales. My work is driven by the will of making complicated things easy and pleasant to interact with.<br/>
                     <br/>
                     I now specialise in web development but my knowledge of multiple fields allows me to work on a project with the full scope in mind; simplifying the process of working together and producing a more cohesive experience for your clients and users. Feel free to contact me or browse my projects in my portfolio.
