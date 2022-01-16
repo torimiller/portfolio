0b6e165603a65eae6ad1bac18b764d63bced1ec4
Tori Miller <millertori04@gmail.com>
2022-01-16 11:29:47 -0800

    updated bio

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -10,10 +10,14 @@ const About = () => {
                 <div className="about-col-left">
                     <h1 className="about-h1">Full stack web developer with experience 
                     building web applications from scratch, developing and integrating APIs, and working with databases.</h1>
-                    <p className="about-body-copy">I’m a well-organized, dedicated, and versatile developer with a strong passion for technology and design. I’ve worn many hats in my career between design, marketing, project management and sales. My work is driven by the will of making complicated things easy and pleasant to interact with.<br/>
+                    <p className="about-body-copy">I’m a well-organized, dedicated, and versatile developer with the capability 
+                        to learn and collaborate in rapidly changing environments. I now specialize in web development but my 
+                        experience in multiple fields including design, marketing, and project management allows me to work on a 
+                        project with the full scope in mind. I have a proven ability to adapt in both self-starting and collaborative 
+                        environments while staying focused on achieving high-quality results under strict deadlines.<br/>
                     <br/>
-                    I now specialise in web development but my knowledge of multiple fields allows me to work on a project with the full scope in mind; simplifying the process of working together and producing a more cohesive experience for your clients and users. Feel free to contact me or browse my projects in my portfolio.
-                    </p>
+                    Outside of the office you’ll find me working on various sewing projects, wrangling my dog during a hike, or 
+                    dancing terribly at a concert.                    </p>
                     <div className="contact-wrapper">
                         <p>Are you looking for a full stack developer? I'd love to chat. 
                         Send me a message at <a className="contact-email" href="mailto:torimillerpdx@gmail.com">torimillerpdx@gmail.com</a> and let's connect.</p>
@@ -28,10 +32,7 @@ const About = () => {
                             <li className="skills-li">Responsive HTML/CSS</li>
                             <li className="skills-li">Javascript (vanilla, React/Redux, jQuery)</li>
                         <li className="li-header">Testing Tools</li>
-                            <li className="skills-li">Jest</li>
-                            <li className="skills-li">Enzyme</li>
-                            <li className="skills-li">Mocha</li>
-                            <li className="skills-li">Chai</li>
+                            <li className="skills-li">Unit testing with Jest, Enzyme, Mocha, and Chai</li>
                         <li className="li-header">API Integration & Development</li>
                             <li className="skills-li">REST API integration</li>
                             <li className="skills-li">REST API development with Node.js</li>
